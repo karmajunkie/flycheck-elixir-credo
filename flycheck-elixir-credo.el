610364a06e7393fbca8efb5e725e6a9ef04e8811
Gerard de Brieder <smeevil@me.com>
2016-03-03 20:17:46 +0100

    do not chain for now

--- flycheck-elixir-credo.el
+++ flycheck-elixir-credo.el
@@ -49,7 +49,7 @@
    (warning line-start (file-name) ":" line ": " (or "C" "W")  ": " (message) line-end)
    )
   :modes (elixir-mode)
-  :next-checkers ((warning . elixir-dogma))
+  ;;:next-checkers ((warning . elixir-dogma))
 )
 
 ;;;###autoload
